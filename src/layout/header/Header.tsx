8b4e2b1ae4adbce4f85f86b01e6af4687a9058f4
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-01-03 16:55:35 +0300

    Added layout component

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from "styled-components";
+import {Logo} from "../../components/logo/Logo";
+import {Menu} from "../../components/menu/Menu";
+import {Social} from "../../components/social/Social";
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <Logo/>
+            <Menu/>
+            <Social/>
+        </StyledHeader>
+    );
+};
+
+const StyledHeader = styled.header`
+    display: flex;
+    justify-content: space-between;
+    background-color: greenyellow;
+`
