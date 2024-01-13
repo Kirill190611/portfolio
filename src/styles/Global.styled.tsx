c9fae768a33a7c03d3cfebfd07ecf23ec46e6cd4
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-01-13 19:45:55 +0300

    Added global styles for project

---
+++ src/styles/Global.styled.tsx
@@ -0,0 +1,37 @@
+import {createGlobalStyle} from "styled-components";
+import {Theme} from "./Theme.styled";
+
+export const GlobalStyled = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+    
+    body {
+        margin: 0;
+        font-family: 'Jost', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: ${Theme.lightTheme.textColors.primary};
+        background-color: ${Theme.lightTheme.backgroundColors.primary};
+        font-size: ${Theme.lightTheme.fontSize.primary};
+    }
+    
+    a {
+        text-decoration: none;
+    }
+    
+    ul {
+        list-style: none;
+    }
+    
+    button {
+        background-color: unset;
+        border: none;
+    }
+`
---
+++ src/styles/Theme.styled.tsx
@@ -0,0 +1,33 @@
+export const Theme = {
+    lightTheme: {
+        textColors: {
+            primary: "#666666",
+            secondary: "#42446E",
+            lightContent: "#A7A7A7",
+            darkContent: "#000000"
+        },
+        textAccentColors: {
+            primaryAccent: "#13B0F5",
+            secondaryAccent: "#018C0F"
+        },
+        titleColors: {
+            primary: "#42446E",
+            secondary: "#000000"
+        },
+        backgroundColors: {
+            primary: "#FFFFFF",
+            accent: "#D7FFE0"
+        },
+        fontSize: {
+            primary: "20px",
+            secondary: "18px",
+            primaryAccent: "58px",
+            secondaryAccent: "32px"
+        },
+        titleFontSize: {
+            primary: "58px",
+            secondary: "48px",
+            projectsTitle: "28px"
+        }
+    }
+}
