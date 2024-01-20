56292d3df43b2c211f0c5ef70657968b8be8a411
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-01-20 16:58:09 +0300

    Warning fixed

--- src/layout/sections/contact/Contact.tsx
+++ src/layout/sections/contact/Contact.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {ContactDescription} from "../../../components/descriptions/ContactDescription";
 import {ContactMail} from "../../../components/contacts/ContactMail";
-import {SecondTitle} from "../../../components/titles/SecondTitle";
 import styled from "styled-components";
 import {Container} from "../../../components/Container";
 
